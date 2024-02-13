26e3b2d056eed03a7e0c5882052a7245d33392ac
Jon3sjns <Jon3sjns@github.com>
2024-02-13 14:47:31 +0700

    updated: headers duckduckgo

--- search.py
+++ search.py
@@ -205,21 +205,7 @@ class Main:
         if req_headers == "duckduckgo":
             header = {
             'Authority': 'html.duckduckgo.com',
-            'Method': 'GET',
-            'Scheme': 'https',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Accept-Language': 'en-US,en;q=0.9,id;q=0.8',
-            'Cache-Control': 'max-age=0',
-            'Cookie': 'kl=be-fr',
-            'Sec-Ch-Ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"Linux"',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
             }
         elif req_headers == "google":
             header = {"User-Agent": self.ua()}
